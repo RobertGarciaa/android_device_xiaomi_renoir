c166de648006088a17708e9210d992ed7c62a6d9
RobertGarciaa <chae0218@naver.com>
2023-07-01 09:08:45 +0900

    sm8350-common: Bringup havoc
    Signed-off-by: RobertGarciaa <chae0218@naver.com>

--- lineage_renoir.mk
+++ lineage_renoir.mk
@@ -11,21 +11,17 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from renoir device
 $(call inherit-product, device/xiaomi/renoir/device.mk)
 
-# Inherit some common evolutionFest stuff.
-$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
+# Inherit some common HavocOS stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := renoir
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := M2101K9G
-PRODUCT_NAME := evolution_renoir
-
-EVO_SIGNED := false
-TARGET_SUPPORTS_QUICK_TAP := true
-TARGET_BOOT_ANIMATION_RES := 1080
+PRODUCT_NAME := lineage_renoir
 
 TARGET_GAPPS_ARCH := arm64
-TARGET_USES_MINI_GAPPS := true
+HAVOC_MAINTAINER := Alps Yogurt
 
 PRODUCT_SYSTEM_NAME := renoir_global
 PRODUCT_SYSTEM_DEVICE := renoir
